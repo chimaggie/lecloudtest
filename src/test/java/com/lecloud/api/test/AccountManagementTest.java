814cdbb7845fd43d70b02ef16f388179e700ba19
Maven Lu <maven@houzz.com>
2016-04-20 00:44:54 -0700

    simplified test framework

--- src/test/java/com/lecloud/api/test/AccountManagementTest.java
+++ src/test/java/com/lecloud/api/test/AccountManagementTest.java
@@ -1,43 +1,15 @@
 package com.lecloud.api.test;
 
-import com.lecloud.api.framework.ApiTestSuite;
-import com.lecloud.api.framework.ApiTestSuiteParameterProvider;
+import com.faradayfuture.test.commons.rest.runner.DefaultRestTestClass;
+import com.faradayfuture.test.commons.rest.runner.RestTestGroupJsonPath;
 import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
-import junitparams.naming.TestCaseName;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * Created by maven on 4/18/16.
+ * Created by maggie on 4/19/16.
  */
 @RunWith(JUnitParamsRunner.class)
-public class AccountManagementTest {
-    private static ApiTestSuite suite = ApiTestSuite.build(AccountManagementTest.class);
+@RestTestGroupJsonPath("account-management.json")
+public class AccountManagementTest extends DefaultRestTestClass {
 
-    @BeforeClass
-    public static void beforeClass() throws Exception {
-        suite.beforeClassConfig();
-    }
-
-    @Test
-//    @Parameters(source = ApiTestSuiteParameterProvider.class)
-    @Parameters(method = "params")
-    @TestCaseName("test - {0}")
-    public void apiTest(String apiTestName) throws Exception {
-        suite.beforeTestConfig();
-        suite.asserting(suite.getTests().get(apiTestName));
-        suite.afterTestConfig();
-    }
-
-    private Object params() {
-        return suite.getTests().keySet().toArray(new Object[]{});
-    }
-
-    @AfterClass
-    public static void afterClass() throws Exception {
-        suite.afterClassConfig();
-    }
 }
